bc208cacba6f806361d503b7eff959a558b5df0e
Ilya Martynov <martynov97@gmail.com>
2024-03-26 12:01:11 +0300

    fixed lint in brain-prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -4,6 +4,19 @@ from random import randint
 import prompt
 
 
+def is_prime_number(guess_number):
+    divider = 2
+    if guess_number == 1:
+        return 'no'
+    while divider <= guess_number:
+        if guess_number % divider != 0:
+            divider += 1
+        elif guess_number == divider and guess_number != 1:
+            return 'yes'
+        elif guess_number % divider == 0:
+            return 'no'
+
+
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
@@ -11,27 +24,9 @@ def main():
     print('Answer \"yes\" if given number is prime. Otherwise answer \"no\".')
     tries = 0
     while tries < 3:
-        divider = 2
-        result = ""
         guess_number = randint(1, 100)
+        result = is_prime_number(guess_number)
         answer = str(input(f'Question: {guess_number}\nYour answer: '))
-        if guess_number == 1 and answer == 'no':
-            print('Correct!')
-            tries += 1
-        elif guess_number == 1 and answer != 'no':
-            print(f'{answer} is wrong answer ;(. Correct answer was \"no\".')
-            print(f'Let\'s try again, {name}!')
-            break
-        while divider <= guess_number:
-            if guess_number % divider != 0:
-                divider += 1
-            elif guess_number == divider:
-                result = 'yes'
-                break
-            elif guess_number % divider == 0:
-                divider += 1
-                result = 'no'
-                break
         if answer == result:
             print('Correct!')
             tries += 1
